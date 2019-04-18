1d33f87b36cd7654d51ae1aba004a0e82b62c282
Henk Tjalsma <G00376321@gmit.ie>
2019-04-18 17:46:57 +0100

    Changed layout plot script, 180419:17:46

--- plot_iris_dataset.py
+++ plot_iris_dataset.py
@@ -5,16 +5,14 @@
 =========================================================
 The Iris Dataset
 =========================================================
-This data sets consists of 3 different types of irises'
-(Setosa, Versicolour, and Virginica) petal and sepal
-length, stored in a 150x4 numpy.ndarray
+This data sets consists of 3 different types of irises' (Setosa, Versicolour, and Virginica) petal and sepal length, 
+stored in a 150x4 numpy.ndarray
 
 The rows being the samples and the columns being:
 Sepal Length, Sepal Width, Petal Length and Petal Width.
 
 The below plot uses the first two features.
-See `here <https://en.wikipedia.org/wiki/Iris_flower_data_set>`_ for more
-information on this dataset.
+See `here <https://en.wikipedia.org/wiki/Iris_flower_data_set>`_ for more information on this dataset.
 """
 print(__doc__)
 
